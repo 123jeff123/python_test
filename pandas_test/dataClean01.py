160ea9bd6fb21218ffeca7c71d452a38387401aa
minglu <ming.lu.fan.dl@ibm.com>
2023-03-21 15:31:45 +0800

    init csv

---
+++ pandas_test/dataClean01.py
@@ -0,0 +1,10 @@
+# import pandas as pd
+#
+# data_url = 'C:\\Users\\MingFan\\Desktop\\python01\\data01.csv'
+# df = pd.read_csv(data_url)
+#
+# # 删除重复行
+# df.drop_duplicates(inplace=True)
+# df.fillna(0,inplace=True)
+#
+# print(df)
